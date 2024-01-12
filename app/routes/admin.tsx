909a0356624c5dcd0aca7e23476c9f86aaca749e
fcablik <filip.cablik@icloud.com>
2024-01-12 19:45:21 +0100

    fix overflowing sidebar to full width on hover / or on fixed 152px width -> function for allowing full width only on hover, otherwise fixed 152px

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,5 +1,6 @@
 import { type DataFunctionArgs } from '@remix-run/node'
 import { Link, NavLink, Outlet } from '@remix-run/react'
+import { useState } from 'react'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
@@ -50,7 +51,7 @@ function SidebarMainNavLink({
 								className={cn(
 									'no-scrollbar overflow-x-scroll text-sm max-lg:text-xs',
 									!isActive
-										? 'group-hover/rooms:opacity-100 lg:opacity-0 lg:transition-opacity lg:duration-300 lg:group-hover/item:opacity-100 group-hover/item:pointer-events-auto'
+										? 'group-hover/item:pointer-events-auto group-hover/rooms:opacity-100 lg:opacity-0 lg:transition-opacity lg:duration-300 lg:group-hover/item:opacity-100'
 										: '',
 								)}
 							>
@@ -123,11 +124,31 @@ export default function AdminRoute() {
 	const sidebarBoxBaseClasslist =
 		'lg:flex lg:flex-col lg:justify-between lg:items-center rounded-3xl bg-foreground text-background'
 
+	const [isHovered, setIsHovered] = useState(false)
+
+	const handleMouseEnter = () => {
+		setIsHovered(true)
+	}
+
+	const handleMouseLeave = () => {
+		setTimeout(() => {
+			setIsHovered(false)
+		}, 300)
+	}
 	return (
 		<div className="flex items-start justify-center">
 			<div className="max-lg:contents">
-				<div className="fixed z-3001 w-full max-lg:bottom-0 lg:h-full lg:overflow-y-scroll">
-					<div className="pb-2 max-lg:px-2 lg:w-[152px] lg:pb-20 lg:pr-9 xl:py-6 2xl:py-10">
+				<div
+					className={cn(
+						'fixed z-3001 w-full max-lg:bottom-0 lg:h-full lg:overflow-y-scroll',
+						!isHovered && 'lg:w-[152px]',
+					)}
+				>
+					<div
+						className="pb-2 max-lg:px-2 lg:w-[152px] lg:pb-20 lg:pr-9 xl:py-6 2xl:py-10"
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}
+					>
 						<div className={cn(sidebarBoxBaseClasslist, 'py-2 lg:py-10')}>
 							<div className="text-center max-lg:hidden">logo</div>
 
@@ -148,8 +169,8 @@ export default function AdminRoute() {
 								<div className="group/rooms relative w-full">
 									<SidebarMainNavLink routeName="rooms" icon="home" />
 
-									<div className="group-hover/rooms:pointer-events-auto pointer-events-none absolute z-3001 max-lg:right-0 max-lg:bottom-16 lg:left-full lg:top-[-2rem]">
-										<div className="ml-4 opacity-0 rounded-2xl bg-foreground px-4 py-2 transition group-hover/rooms:opacity-100">
+									<div className="pointer-events-none absolute z-3001 group-hover/rooms:pointer-events-auto max-lg:bottom-16 max-lg:right-0 lg:left-full lg:top-[-2rem]">
+										<div className="ml-4 rounded-2xl bg-foreground px-4 py-2 opacity-0 transition group-hover/rooms:opacity-100">
 											<SidebarNavLink
 												routeName="rooms/pricing"
 												title="pricings"
@@ -192,8 +213,8 @@ export default function AdminRoute() {
 										className="w-full cursor-pointer"
 									/>
 
-									<div className="group-hover/additional:pointer-events-auto pointer-events-none absolute z-3001 max-lg:right-0 max-lg:bottom-16 lg:left-full lg:bottom-[-5rem]">
-										<div className="ml-4 opacity-0 rounded-2xl bg-foreground px-4 py-2 transition group-hover/additional:opacity-100">
+									<div className="pointer-events-none absolute z-3001 group-hover/additional:pointer-events-auto max-lg:bottom-16 max-lg:right-0 lg:bottom-[-5rem] lg:left-full">
+										<div className="ml-4 rounded-2xl bg-foreground px-4 py-2 opacity-0 transition group-hover/additional:opacity-100">
 											<SidebarNavLink
 												routeName="pages"
 												icon="file-text"
